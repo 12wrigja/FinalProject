d68f82e37ae7423a7758feee2af7bff1e075cd18
James Wright <James@Jamess-MacBook-Pro.local>
2015-02-06 01:24:44 -0500

    Update to add in the correct Git ignore and setup a script for people to record their names.
    Signed-off-by: James Wright <James@Jamess-MacBook-Pro.local>

---
+++ Assets/Scripts/NameScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*
+ * Names: James Wright
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class NameScritp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
