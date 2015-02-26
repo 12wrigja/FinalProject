8af700ad3a2af905f7bdb39f7ce6118eef18bf46
James Wright <jow5@case.edu>
2015-02-26 14:00:31 -0500

    Update to ui system to start work on debug menu.
    Signed-off-by: James Wright <jow5@case.edu>

--- Assets/Scripts/GUI/UIManager.cs
+++ Assets/Scripts/GUI/UIManager.cs
@@ -5,20 +5,27 @@ using System.Collections.Generic;
 //The menu manager controls the switching between UI menu's.
 public class UIManager : MonoBehaviour {
 
-    public KeyCode debugListKeyCode;
+    public KeyCode debugListKeyCode = KeyCode.BackQuote;
 
     private static UIManager instance;
 
     private static UIElement[] allUIElements;
     private static List<UIElement> currentUIElements;
 
+    private static UIElement debugUIPanel;
+
 	public void Start(){
         allUIElements = GetComponentsInChildren<UIElement>();
+        currentUIElements = new List<UIElement>();
+        debugUIPanel = allUIElements[allUIElements.Length - 1];
 	}
 
     public void Update()
     {
-
+        if (Input.GetKeyDown(debugListKeyCode))
+        {
+            ToggleUIElement(debugUIPanel);
+        }
     }
 
 	//Show menu shows the current menu. If null is passed it, it shows nothing while disabling the previous menu.
@@ -34,6 +41,12 @@ public class UIManager : MonoBehaviour {
         element.isOnScreen = true;
     }
 
+    public static void HideUIElement(UIElement element)
+    {
+        currentUIElements.Remove(element);
+        element.isOnScreen = false;
+    }
+
     public static void clearScreen()
     {
         if (currentUIElements.Count > 0)
@@ -41,7 +54,20 @@ public class UIManager : MonoBehaviour {
             foreach (UIElement element in currentUIElements)
             {
                 element.isOnScreen = false;
+                currentUIElements.Remove(element);
             }
+        }
+    }
+
+    private static void ToggleUIElement(UIElement element)
+    {
+        if (element.isOnScreen)
+        {
+            HideUIElement(element);
+        }
+        else
+        {
+            ShowUIElement(element);
         }
     }
 }
