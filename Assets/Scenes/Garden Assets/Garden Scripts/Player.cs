5347a515d0079be63fc22a6e512382e5ddec2845
Drake Poling <dsp45@case.edu>
2015-03-03 14:42:41 -0500

    Made the shovel move
    kind of follows the mouse

---
+++ Assets/Scenes/Garden Assets/Garden Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public GameObject shovel;
+	public Vector2 screenSize;
+	public Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = new Vector3(Input.mousePosition.x / screenSize.x, shovel.transform.position.y, Input.mousePosition.y / screenSize.y);
+		shovel.transform.position = newPosition - new Vector3(screenSize.x - offset.x, 0f, screenSize.y - offset.y); 
+		Debug.Log ("mouse position: " + Input.mousePosition.x + ", " + Input.mousePosition.y);
+		//Debug.Log ("shovel position: " + shovel.transform.position.x + ", " +shovel.transform.position.y);
+	}
+}
