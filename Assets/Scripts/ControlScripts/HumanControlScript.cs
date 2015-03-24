9455a4cbb0833fcd7faedbdd30f14164d44b7563
James Wright <12wrigja@gmail.com>
2015-03-24 14:11:52 -0400

    Update to add in a new character to the demo who we will be able to move around and control, will be used to interact with the other characters. Also added in support for Visual Studio 2013 tools.
    Signed-off-by: James Wright <12wrigja@gmail.com>

---
+++ Assets/Scripts/ControlScripts/HumanControlScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanControlScript : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Forward_Axis", v);
+        anim.SetFloat("Rotate_Axis", h);
+	}
+}
