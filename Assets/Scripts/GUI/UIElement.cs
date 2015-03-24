6dac5cd744d74139eac34932e216e37e36405149
James Wright <12wrigja@gmail.com>
2015-03-24 13:22:14 -0400

    Update to load in the UI package into the demo.
    Signed-off-by: James Wright <12wrigja@gmail.com>

---
+++ Assets/Scripts/GUI/UIElement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//The Menu class represents the state of a UI menu and interfaces the code with the animator.
+public class UIElement : MonoBehaviour {
+
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+    [SerializeField]
+	//Determines if the menu is open on the screen
+	public bool isOnScreen{
+		get{
+			return _animator.GetBool("IsOpen");
+		}
+		set{
+			_animator.SetBool("IsOpen",value);
+		}
+	}
+
+	void Awake(){
+		_animator = GetComponent<Animator>();
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0,0);
+	}
+
+	void Update(){
+		if(_animator.GetCurrentAnimatorStateInfo(0).IsName("Open")){
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+		} else{
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+		}
+	}
+}
