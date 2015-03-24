7fc19a6b3d5c48452fad4bebbbf29aabb9e6623b
Joel Kalos <jdk138@case.edu>
2015-03-24 15:31:13 -0400

    Added whale/fish asset

--- Assets/Scripts/Fishing/FishingManager.cs
+++ Assets/Scripts/Fishing/FishingManager.cs
@@ -42,10 +42,6 @@ public class FishingManager : MonoBehaviour {
 				canCatch = true;
 			}
 		}
-//
-//		if(Input.GetKey(KeyCode.Space) && canFish && !fishCaught){
-//			isFishing = true;
-//		}
 
 	}
 
