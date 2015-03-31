f67e761dd217905ddcbeed585906b2f07fff1ae8
Haley <hne3@case.edu>
2015-03-31 14:57:44 -0400

    Test of key-response animations
    Mrs. Scientist should shift to the left when you press the space bar,
    and return to idle when you press it again. You can do this as many
    times as you want.

---
+++ Assets/Scripts/TurnCondition.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnCondition : MonoBehaviour {
+
+	public Animator animator;
+	public bool isPressed = false;
+	private bool currentlyTurned = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Space") && !currentlyTurned){
+			isPressed = true;
+			currentlyTurned = true;
+		}
+		else if(Input.GetButtonDown("Space") && currentlyTurned){
+			isPressed = false;
+			currentlyTurned = false;
+		}
+		animator.SetBool ("isPressed", isPressed);
+	}
+}
