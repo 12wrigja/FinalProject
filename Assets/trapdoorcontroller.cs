072496a6f45c15af4191d85c1d975c0b57121798
James Wright <jow5@case.edu>
2015-04-28 13:01:41 -0400

    Update to start working on the trapdoor appearing, as well as setting up the basement area.

---
+++ Assets/trapdoorcontroller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trapdoorcontroller : MonoBehaviour {
+
+    public GameObject trapDoor;
+    private Animator trapDoorAnimator;
+
+    public bool foundKey;
+
+	// Use this for initialization
+	void Start () {
+        foundKey = false;
+        trapDoorAnimator = trapDoor.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (foundKey)
+        {
+            trapDoorAnimator.SetTrigger("RiseTrigger");
+        }
+	}
+
+    void HaveFoundKey()
+    {
+        foundKey = true;
+    }
+}
